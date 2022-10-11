5e118276f7a4774ba9f8b70c2b7b926a2239b616
sebastianAksamitowski <s.aksamitowski@gmail.com>
2022-10-11 11:24:21 +0200

    nav close

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import ClickAwayListener from 'react-click-away-listener';
 
 import { nav } from '../data';
 import Logo from '../assets/img/logo.svg';
@@ -21,48 +22,51 @@ const Header = () => {
         bg ? 'bg-white shadow-lg py-5' : 'py-9'
       } fixed left-0 right-0 z-50 transition-all duration-300`}
     >
-      <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
-          <Link
-            to='home'
-            className='cursor-pointer transition-all'
-            smooth={true}
-            duration={500}
-          >
-            <img src={Logo} alt=''></img>
-          </Link>
-          <ul
-            className={`${
-              showNav ? 'left-0' : '-left-full'
-            } flex flex-col justify-center items-center bg-accent fixed top-0 text-white text-xl capitalize w-80 h-full gap-y-8 duration-200 transition-all`}
-          >
-            {nav.map((item, index) => {
-              return (
-                <li className='cursor-pointer' key={index}>
-                  <Link
-                    to={item.name}
-                    activeClass='active'
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
-                    className='transition-all'
-                  >
-                    {item.name}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-          <div onClick={() => setShowNav(!showNav)}>
-            {showNav ? (
-              <RiCloseFill className='w-8 h-8' />
-            ) : (
-              <RiBarChartHorizontalLine className='w-8 h-8' />
-            )}
+      <ClickAwayListener onClickAway={() => setShowNav(false)}>
+        <div className='container mx-auto'>
+          <div className='flex justify-between items-center'>
+            <Link
+              to='home'
+              className='cursor-pointer transition-all'
+              smooth={true}
+              duration={500}
+            >
+              <img src={Logo} alt=''></img>
+            </Link>
+            <ul
+              className={`${
+                showNav ? 'left-0' : '-left-full'
+              } flex flex-col justify-center items-center bg-accent fixed top-0 text-white text-xl capitalize w-80 h-full gap-y-8 duration-200 transition-all`}
+            >
+              {nav.map((item, index) => {
+                return (
+                  <li className='cursor-pointer' key={index}>
+                    <Link
+                      onClick={() => setShowNav(!showNav)}
+                      to={item.name}
+                      activeClass='active'
+                      spy={true}
+                      smooth={true}
+                      offset={-20}
+                      duration={500}
+                      className='transition-all'
+                    >
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+            <div onClick={() => setShowNav(!showNav)}>
+              {showNav ? (
+                <RiCloseFill className='w-8 h-8' />
+              ) : (
+                <RiBarChartHorizontalLine className='w-8 h-8' />
+              )}
+            </div>
           </div>
         </div>
-      </div>
+      </ClickAwayListener>
     </header>
   );
 };
